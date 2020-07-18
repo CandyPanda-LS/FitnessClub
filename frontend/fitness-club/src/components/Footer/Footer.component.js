dd0f9386736e63cdee7bb94deff8441a42baa530
CandyPanda-LS <62637475+CandyPanda-LS@users.noreply.github.com>
2020-07-18 23:13:37 +0530

    test

--- frontend/fitness-club/src/components/Footer/Footer.component.js
+++ frontend/fitness-club/src/components/Footer/Footer.component.js
@@ -8,7 +8,7 @@ const Footer = () => {
       <footer class="bg-white sticky-footer">
         <div class="container my-auto">
           <div class="text-center my-auto copyright">
-            <span>Copyright © Fitness-Club 2020</span>
+            <span>Copyright © Fitness-Club 2020-2021</span>
           </div>
         </div>
       </footer>
