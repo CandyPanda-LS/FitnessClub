b06913a9a7e68b742cc4ea318d0da682c4a8f804
SenuraJayadeva <senurajayadeva@gmail.com>
2020-09-07 22:09:42 +0530

    updated

--- backend/routes/api/advertisement.js
+++ backend/routes/api/advertisement.js
@@ -1,34 +1,34 @@
-const express = require('express');
-const fileUpload = require('express-fileupload');
+const express = require("express");
+const fileUpload = require("express-fileupload");
 const router = express.Router();
-const auth = require('../../middleware/auth');
-const { check, validationResult } = require('express-validator');
-const Advertisement = require('../../models/Advertisement');
-const cors = require('cors');
-const path = require('path'); //for seting path
+const auth = require("../../middleware/auth");
+const { check, validationResult } = require("express-validator");
+const Advertisement = require("../../models/Advertisement");
+
+const path = require("path"); //for seting path
 const dirPath = path.join(
   __dirname,
-  '../../../frontend/fitness-club/public/uploads'
+  "../../../frontend/fitness-club/public/uploads"
 ); //for seting path
 
 const app = express();
+const cors = require("cors");
+router.use(cors());
 
 app.use(fileUpload()); //for image uploading
 
-router.use(cors());
-
 //test path
-router.get('/path', (req, res) => {
-  console.log('path is  = ' + dirPath);
+router.get("/path", (req, res) => {
+  console.log("path is  = " + dirPath);
 });
 
 //@route  POST api/advertisement/
 //@desc   Add advertisement into the database
 //@access Private
 //to protect auth add as the second parameter
-router.post('/', async (req, res) => {
+router.post("/", async (req, res) => {
   if (req.files == null) {
-    return res.status(400).json({ msg: 'No file uploaded' });
+    return res.status(400).json({ msg: "No file uploaded" });
   }
 
   const file = req.files.file;
@@ -52,31 +52,62 @@ router.post('/', async (req, res) => {
 
     newAdvertisement
       .save()
-      .then(() => res.json('Advertisement Added'))
-      .catch((err) => res.status(400).json('Error: ' + err));
+      .then(() => res.json("Advertisement Added"))
+      .catch((err) => res.status(400).json("Error: " + err));
   });
 });
 
 // @route         GET /advertiesement
 // @description   get advertiesement
 // @access        public
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
   Advertisement.find()
     .then((post) => {
       res.json(post);
     })
-    .catch((err) => res.status(400).json('Error: ' + err));
+    .catch((err) => res.status(400).json("Error: " + err));
 });
 
-// //@route  DELETE api/advertiesement
-// //@desc  Delete Item
-// //@access Private
-// //@author Ayodya
-router.delete('/:id', async (req, res) => {
-  Advertisement.findByIdAndDelete(req.params.id)
-    .then(() => {
-      res.json('Advertisement Deleted');
-    })
-    .catch((err) => res.status(400).json('Error: ' + err));
+// // //@route  DELETE api/advertiesement
+// // //@desc  Delete Item
+// // //@access Private
+// // //@author Ayodya
+// router.delete('/:id', async (req, res) => {
+//   Advertisement.findByIdAndDelete(req.params.id)
+//     .then(() => {
+//       res.json('Advertisement Deleted');
+//     })
+//     .catch((err) => res.status(400).json('Error: ' + err));
+// });
+
+//@route  DELETE api/instructor/meal
+//@desc  Delete Meal
+//@access Private
+//@author Senura
+
+router.delete("/adremove/:deleteadvertisement_id", async (req, res) => {
+  try {
+    //GET remove index
+    Advertisement.findByIdAndDelete(req.params.deleteadvertisement_id)
+      .then(() => {
+        res.json("Advertisement Deleted");
+      })
+      .catch((err) => res.status(400).json("Error: " + err));
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
 });
+
+// router.delete("/", async (req, res) => {
+//   try {
+//     //Remove Advertisement
+//     await Advertisement.findOneAndRemove({ _id: req.body.id });
+
+//     res.json({ msg: "Advertisement Deleted" });
+//   } catch (err) {
+//     console.log(err.message);
+//     res.status(500).send("Server Error");
+//   }
+// });
 module.exports = router;
--- frontend/fitness-club/src/components/Advertisement/advertiesment_view/advertiesment.component.js
+++ frontend/fitness-club/src/components/Advertisement/advertiesment_view/advertiesment.component.js
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import Background from './image/3584.jpg';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Background from "./image/3584.jpg";
 
 export default function AdvertisementComponent() {
   const [advertiesementPost, setAdvertiesement] = useState([]);
@@ -9,17 +9,17 @@ export default function AdvertisementComponent() {
     const sendData = async () => {
       try {
         await axios
-          .get('http://localhost:5000/api/advertisement')
+          .get("http://localhost:5000/api/advertisement")
           .then((res) => {
             console.log(res);
             setAdvertiesement(res.data);
             // setLoading(false);
           })
           .catch((error) => {
-            console.log('No data');
+            console.log("No data");
           });
       } catch (error) {
-        console.log('No Data CATCH');
+        console.log("No Data CATCH");
       }
     };
 
@@ -31,58 +31,58 @@ export default function AdvertisementComponent() {
       <div
         style={{
           backgroundImage: `url(${Background})`,
-          backgroundRepeat: 'no-repeat',
-          backgroundSize: 'cover',
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "cover",
         }}
       >
-        <div class='container'>
+        <div class="container">
           <div
-            class='row justify-content-center'
+            class="row justify-content-center"
             style={{
-              filter: 'blur(0px)',
-              backgroundColor: 'rgba(255,0,0,0)',
-              borderRadius: '0px',
-              borderWidth: '5px',
-              borderBottom: '0px solid rgb(255,255,255)',
+              filter: "blur(0px)",
+              backgroundColor: "rgba(255,0,0,0)",
+              borderRadius: "0px",
+              borderWidth: "5px",
+              borderBottom: "0px solid rgb(255,255,255)",
             }}
           >
             {advertiesementPost.map(function (advertisements) {
               return (
                 <div
-                  class='col-sm-4 col-md-7 col-lg-7 col-xl-7 offset-xl-0'
+                  class="col-sm-4 col-md-6 col-lg-6 col-xl-7 offset-xl-0"
                   style={{
-                    margin: '15px',
-                    borderRadius: '0',
-                    borderWidth: '0px',
-                    filter: 'invert(0%)',
+                    margin: "15px",
+                    borderRadius: "0",
+                    borderWidth: "0px",
+                    filter: "invert(0%)",
                   }}
                 >
                   <div
-                    class='card border rounded-0 shadow-sm d-xl-flex justify-content-xl-center align-items-xl-center'
-                    data-bs-hover-animate='pulse'
+                    class="card border rounded-0 shadow-sm d-xl-flex justify-content-xl-center align-items-xl-center"
+                    data-bs-hover-animate="pulse"
                     style={{
-                      boxShadow: '4px 4px 14px 3px rgba(158,159,166,0.7)',
-                      margin: '13px',
+                      boxShadow: "4px 4px 14px 3px rgba(158,159,166,0.7)",
+                      margin: "13px",
                     }}
                   >
-                    <div class='card'>
+                    <div class="card">
                       <img
-                        class='img-thumbnail img-fluid card-img w-100 d-block d-xl-flex justify-content-xl-center align-items-xl-center'
-                        src={'uploads/' + advertisements.advertiesementImage}
+                        class="img-thumbnail img-fluid card-img w-100 d-block d-xl-flex justify-content-xl-center align-items-xl-center"
+                        src={"uploads/" + advertisements.advertiesementImage}
                         style={{
-                          fontSize: '10px',
+                          fontSize: "10px",
                           filter:
-                            'blur(0px) brightness(100%) grayscale(0%) hue-rotate(0deg) invert(0%) saturate(100%) sepia(0%)',
-                          margin: '0px',
-                          padding: '0px',
+                            "blur(0px) brightness(100%) grayscale(0%) hue-rotate(0deg) invert(0%) saturate(100%) sepia(0%)",
+                          margin: "0px",
+                          padding: "0px",
                         }}
                       />
                     </div>
-                    <div class='card-body'>
-                      <h4 class='card-title' style={{ fontSize: '18px' }}>
+                    <div class="card-body">
+                      <h4 class="card-title" style={{ fontSize: "18px" }}>
                         {advertisements.title}
                       </h4>
-                      <p class='card-text' style={{ fontSize: '12px' }}>
+                      <p class="card-text" style={{ fontSize: "12px" }}>
                         {advertisements.description}
                       </p>
                     </div>
