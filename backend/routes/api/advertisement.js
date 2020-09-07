fb50aa222424bedba53ac65446f8b085dde9fca3
SenuraJayadeva <senurajayadeva@gmail.com>
2020-09-07 18:30:42 +0530

    Revert "Merge branch 'master' of https://github.com/CandyPanda-LS/FitnessClub"
    This reverts commit d4a8067eb7d18b56382abf426e135f20651f17bb, reversing
    changes made to e150a2065da1501ce446c9e066a790b2ff19f06f.

--- backend/routes/api/advertisement.js
+++ backend/routes/api/advertisement.js
@@ -1,14 +1,14 @@
-const express = require("express");
-const fileUpload = require("express-fileupload");
+const express = require('express');
+const fileUpload = require('express-fileupload');
 const router = express.Router();
-const auth = require("../../middleware/auth");
-const { check, validationResult } = require("express-validator");
-const Advertisement = require("../../models/Advertisement");
-const cors = require("cors");
-const path = require("path"); //for seting path
+const auth = require('../../middleware/auth');
+const { check, validationResult } = require('express-validator');
+const Advertisement = require('../../models/Advertisement');
+const cors = require('cors');
+const path = require('path'); //for seting path
 const dirPath = path.join(
   __dirname,
-  "../../../frontend/fitness-club/public/uploads"
+  '../../../frontend/fitness-club/public/uploads'
 ); //for seting path
 
 const app = express();
@@ -18,17 +18,17 @@ app.use(fileUpload()); //for image uploading
 router.use(cors());
 
 //test path
-router.get("/path", (req, res) => {
-  console.log("path is  = " + dirPath);
+router.get('/path', (req, res) => {
+  console.log('path is  = ' + dirPath);
 });
 
 //@route  POST api/advertisement/
 //@desc   Add advertisement into the database
 //@access Private
 //to protect auth add as the second parameter
-router.post("/", async (req, res) => {
+router.post('/', async (req, res) => {
   if (req.files == null) {
-    return res.status(400).json({ msg: "No file uploaded" });
+    return res.status(400).json({ msg: 'No file uploaded' });
   }
 
   const file = req.files.file;
@@ -52,40 +52,31 @@ router.post("/", async (req, res) => {
 
     newAdvertisement
       .save()
-      .then(() => res.json("Advertisement Added"))
-      .catch((err) => res.status(400).json("Error: " + err));
+      .then(() => res.json('Advertisement Added'))
+      .catch((err) => res.status(400).json('Error: ' + err));
   });
 });
 
 // @route         GET /advertiesement
 // @description   get advertiesement
 // @access        public
-router.get("/", async (req, res) => {
+router.get('/', async (req, res) => {
   Advertisement.find()
     .then((post) => {
       res.json(post);
     })
-    .catch((err) => res.status(400).json("Error: " + err));
+    .catch((err) => res.status(400).json('Error: ' + err));
 });
 
 // //@route  DELETE api/advertiesement
 // //@desc  Delete Item
 // //@access Private
 // //@author Ayodya
-<<<<<<< HEAD
 router.delete('/:id', async (req, res) => {
   Advertisement.findByIdAndDelete(req.params.id)
     .then(() => {
       res.json('Advertisement Deleted');
     })
     .catch((err) => res.status(400).json('Error: ' + err));
-=======
-router.delete("/:id", async (req, res) => {
-  Advertisement.findByIdAndDelete(req.params.id)
-    .then(() => {
-      res.json("Advertisement Deleted");
-    })
-    .catch((err) => res.status(400).json("Error: " + err));
->>>>>>> f71ca87f034a3c4389470cd02eab76681b315719
 });
 module.exports = router;
