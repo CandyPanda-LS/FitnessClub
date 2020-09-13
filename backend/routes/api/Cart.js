032c67c797f7d15a4bb6cfaf58c9d0d830fa03ac
lasal <it19132310@my.sliit.lk>
2020-09-13 19:25:30 +0530

    cart implementation Co-authored-by: Jayadeva A. S. V. it19139036 <it19139036@my.sliit.lk>

---
+++ backend/routes/api/Cart.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const cors = require("cors");
+const router = express.Router();
+const auth = require("../../middleware/auth");
+const { check, validationResult } = require("express-validator");
+const Cart = require("../../models/Cart");
+const User = require("../../models/User");
+const { json } = require("express");
+
+router.use(cors());
+
+
+    router.post(
+        "/",auth,
+        async (req, res) => {
+
+        const activated = "active";
+
+        //build profile object
+        const profileFields = {
+
+        };
+        profileFields.user = req.user.id;
+        if (activated) profileFields.activated = activated;
+
+
+        try {
+            let cart = await Cart.findOne({ user: req.user.id });
+
+            if (cart) {
+            //UPDATE
+            cart = await Cart.findOneAndUpdate(
+                { user: req.user.id },
+                { $set: profileFields },
+                { new: true }
+            );
+
+            return res.json(cart);
+            }
+
+            //Create
+            cart = new Cart(profileFields);
+
+            await cart.save();
+            res.json(cart); //return the profile
+        } catch (err) {
+            console.error(err.message);
+            res.status(500).send("Server Error");
+        }
+        }
+    );
