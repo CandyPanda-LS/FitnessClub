8fcab2683a899603269467b32c7b274ff2d396e0
ayodyabanuka <ayodyabanukafernando@gmail.com>
2020-09-15 22:05:38 +0530

    aboutus update

--- frontend/fitness-club/src/components/AboutUs/aboutus.component.js
+++ frontend/fitness-club/src/components/AboutUs/aboutus.component.js
@@ -3,153 +3,35 @@ import React from 'react';
 export default function AboutUs() {
   return (
     <div>
-      <div>
-        <div
-          class='d-block float-none d-xl-flex flex-fill justify-content-center m-auto justify-content-xl-center'
-          style={{ width: '1224px' }}
-        />
-        <div
-          class='card text-light text-center d-xl-flex align-items-center align-content-center align-self-center order-5 m-auto'
-          style={{
-            width: '1822px',
-            height: '105px',
-            margin: '0px',
-            filter: 'blur(0px) brightness(100%) contrast(100%)',
-          }}
-        />
-        <img
-          class='img-fluid card-img w-100 d-block d-xl-flex justify-content-xl-center'
-          style={{
-            width: '1021px',
-            filter:
-              'blur(0px) brightness(47%) grayscale(27%) hue-rotate(0deg) invert(7%) saturate(101%) sepia(0%)',
-            opacity: '1',
-          }}
-          src='assets/img/shutterstock_2222.jpg'
-        />
-        <div class='card-img-overlay'>
-          <h1
-            class='text-uppercase text-center d-xl-flex justify-content-xl-center'
-            style={{
-              Padding: '180px',
-              fontFamily: 'Bebas Neue',
-              fontSize: '114px',
-              color: 'rgb(255,255,255)',
-            }}
-          >
-            ABOUT
-          </h1>
-          <div>
-            <input
-              class='form-control-plaintext'
-              type='text'
-              value='MY BEST LIFE'
-              readonly=''
-              style={{
-                textAlign: 'center',
-                padding: '66px',
-                fontSize: '42px',
-                color: 'rgb(0,0,0)',
-                fontFamily: 'Bebas Neue',
-                paddingTop: '183px',
-                paddingBottom: '12px',
-              }}
-            />
-          </div>
-          <div>
-            <div>
-              <div
-                class='row'
-                style={{ padding: '0px', paddingTop: '27px', margin: '0px' }}
-              >
-                <div class='col'>
-                  <img src='assets/img/ggym.jpg' width='100%' />
+      <div class="d-block float-none d-xl-flex flex-fill justify-content-center m-auto justify-content-xl-center" style={{width: "993px"}}/>
+                <div class="card text-light text-center d-xl-flex align-items-center align-content-center align-self-center order-5 m-auto" style={{width: "1822px",height: "105px",margin: "0px",filter: "blur(0px) brightness(100%) contrast(100%)"}}/><img class="img-fluid card-img w-100 d-block d-xl-flex justify-content-xl-center" style={{filter: "blur(0px) brightness(38%) grayscale(27%) hue-rotate(0deg) invert(7%) saturate(101%) sepia(0%)",opacity: "1"}} src="assets/img/shutterstock_2222.jpg"/>
+                    <div class="card-img-overlay">
+                        <h1 class="text-uppercase text-center d-xl-flex justify-content-xl-center" style={{padding: "180px",fontFamily: 'Bebas Neue',fontSize: "114px",color: "rgb(255,255,255)"}}>ABOUT</h1></div>
+                        <div><input class="form-control-plaintext" type="text" value="MY BEST LIFE" readonly="" style={{textAlign: "center",padding: "66px",fontSize: "42px",color: "rgb(0,0,0)",fontFamily: 'Bebas Neue',paddingTop: "70px",paddingBottom: "12px"}}/></div>
+                        <div>
+                            <div class="row" style={{padding: "0px",paddingTop: "27px",margin: "0px"}}/>
+                                <div class="col"><img src="assets/img/ggym.jpg" width="100%"/></div>
+                                <div class="col"><input class="form-control-plaintext" type="text" value="OUR VISION" readonly="" style={{textAlign: "center",fontFamily: 'Bebas Neue',fontSize: "55px",color: "rgb(0,0,0)"}}/>
+                                    <p class="text-center" style={{color: "rgb(119,119,119)",height: "186px",margin: "14px",padding: "0px",paddingRight: "23px",paddingBottom: "0px",paddingLeft: "23px",textAlign: "center",fontSize: "19px"}}><br>To Empower the People in the Emerging / Frontier Nations, who are at the bottom of the pyramid, to realize their Best Life.<br><br></p>
+                                </div>
+                            </div>
+                            <div class="row" style={{padding: "0px",paddingTop: "27px"margin: "0px"}}/>
+                                <div class="col"><input class="form-control-plaintext d-xl-flex" type="text" value="OUR MISSION" readonly="" style={{textAlign: "center",fontFamily: 'Bebas Neue',fontSize: "55px",color: "rgb(0,0,0)"}}/>
+                                    <p class="text-center d-xl-flex"
+                                        style={{color: "rgb(119,119,119)",height: "186px",margin: "14px",padding: "0px",paddingRight: "23px",paddingBottom: "0px",paddingLeft: "23px",textAlign: "center",fontSize: "19px"}}/><br>To Extend Lives, Restore Good Health and Enhance Physical Fitness Levels of members so as to nurture them to have a Devoted Heart, a Purposeful Soul and a Courageous Mind.<br><br></p>
+                                </div>
+                                <div class="col"><img src="assets/img/shutterstock_1069150037.jpg" width="100%"></div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div class='col'>
-                  <input
-                    class='form-control-plaintext'
-                    type='text'
-                    value='OUR VISION'
-                    readonly=''
-                    style={{
-                      textAlign: 'center',
-                      fontFamily: 'Bebas Neue',
-                      fontSize: '55px',
-                      color: 'rgb(0,0,0)',
-                    }}
-                  />
-                  <p
-                    style={{
-                      color: 'rgb(119,119,119)',
-                      height: '186px',
-                      margin: '14px',
-                      padding: '0px',
-                      paddingRight: '23px',
-                      paddingBottom: '0px',
-                      paddingLeft: '23px',
-                      textAlign: 'center',
-                      fontSize: '19px',
-                    }}
-                  >
-                    <br />
-                    To Empower the People in the Emerging / Frontier Nations,
-                    who are at the bottom of the pyramid, to realize their Best
-                    Life.
-                    <br />
-                    <br />
-                  </p>
-                </div>
-              </div>
-              <div
-                class='row'
-                style={{ padding: '0px', paddingTop: '27px', margin: '0px' }}
-              >
-                <div class='col'>
-                  <input
-                    class='form-control-plaintext'
-                    type='text'
-                    value='OUR MISSION'
-                    readonly=''
-                    style={{
-                      textAlign: 'center',
-                      fontFamily: 'Bebas Neue',
-                      fontSize: '55px',
-                      color: 'rgb(0,0,0)',
-                    }}
-                  />
-                  <p
-                    style={{
-                      color: 'rgb(119,119,119)',
-                      height: '186px',
-                      margin: '14px',
-                      padding: '0px',
-                      paddingright: '23px',
-                      paddingBottom: '0px',
-                      paddingleft: '23px',
-                      textalign: 'center',
-                      fontsize: '19px',
-                    }}
-                  >
-                    {' '}
-                    <br />
-                    To Extend Lives, Restore Good Health and Enhance Physical
-                    Fitness Levels of members so as to nurture them to have a
-                    Devoted Heart, a Purposeful Soul and a Courageous Mind.
-                    <br />
-                    <br />
-                  </p>
-                </div>
-                <div class='col'>
-                  <img
-                    src='assets/img/shutterstock_1069150037.jpg'
-                    width='100%'
-                  />
-                </div>
-              </div>
             </div>
-          </div>
         </div>
-      </div>
+    </div><a class="border rounded d-inline scroll-to-top" href="#page-top"><i class="fas fa-angle-up"></i></a></div>
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.js"></script>
+    <script src="assets/js/theme.js"></script>
     </div>
   );
 }
