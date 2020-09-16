468473f7f52efca4481b256d12c4b6cf95ba96a6
SenuraJayadeva <senurajayadeva@gmail.com>
2020-09-16 11:25:21 +0530

    fixed item cart plus minus button

--- frontend/fitness-club/src/components/Shop/Pages/Item/Item.component.js
+++ frontend/fitness-club/src/components/Shop/Pages/Item/Item.component.js
@@ -253,7 +253,7 @@ export default class Item extends Component {
                     <br />
                     <br />
                     {/* size picker start                 */}
-                    <div class="row" style={{ marginTop: "0px" }}>
+                    {/* <div class="row" style={{ marginTop: "0px" }}>
                       <div class="col">
                         <Fab
                           color="primary"
@@ -278,7 +278,7 @@ export default class Item extends Component {
                           <AddIcon />
                         </Fab>
                       </div>
-                    </div>
+                    </div> */}
                     {/* size picker end                 */}
                     {/* quantity picker start           */}
                     <div class="row" style={{ marginTop: "0px" }}>
@@ -291,12 +291,21 @@ export default class Item extends Component {
                         >
                           <RemoveIcon />
                         </Fab>
-                        <TextField
-                          id="outlined-basic"
-                          variant="outlined"
+                        &nbsp;&nbsp; &nbsp;
+                        <input
+                          // id="outlined-basic"
+                          // variant="outlined"
+                          style={{
+                            border: "none",
+                            outline: "none",
+                            background: "none",
+                            maxWidth: "30px",
+                            color: "white",
+                          }}
                           value={this.state.ItemQuantity}
                           onChange={this.onChangeQuantity}
                         />
+                        &nbsp; &nbsp; &nbsp;
                         <Fab
                           color="primary"
                           size="small"
@@ -308,6 +317,7 @@ export default class Item extends Component {
                       </div>
                     </div>
                     {/* quantity picker end                 */}
+                    <br />
                     <h1
                       style={{
                         marginTop: "6px",
@@ -318,7 +328,7 @@ export default class Item extends Component {
                         fontWeight: "bold",
                       }}
                     >
-                      {this.state.ItemPrice}
+                      {this.state.ItemPrice * this.state.ItemQuantity} LKR
                     </h1>
                   </div>
                 </div>
@@ -335,7 +345,7 @@ export default class Item extends Component {
                     backgroundColor: "#0e4d6f",
                   }}
                 >
-                  Button
+                  Add to Cart
                 </button>
               </div>
             </div>
